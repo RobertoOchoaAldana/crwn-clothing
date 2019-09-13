82a6d0f81d8d0209739087e87b0cd587227a5ad7
Roberto Ochoa <rochoa@intersysconsulting.com>
2019-09-13 14:32:20 -0500

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
-const addItemToCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id
     );
@@ -13,4 +13,17 @@ const addItemToCart = (cartItems, cartItemToAdd) => {
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 } 
 
-export default addItemToCart;
+export const removeItemFromCart = (cartItems, cartItemToDelete) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToDelete.id
+    );
+
+    if(existingCartItem.quantity>1){
+        return cartItems.map(cartItem =>
+            cartItem.id === existingCartItem.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 } 
+            : cartItem
+        );
+    }
+    return cartItems.filter(item=> item.id !== cartItemToDelete.id);
+}
